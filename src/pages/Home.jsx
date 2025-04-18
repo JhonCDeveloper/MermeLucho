68c7eb7d5aee1164a331dc24383edd1178ae44df
Jhon Cadavid <jairovalencia100k19@mail.com>
2025-04-18 01:33:09 -0500

    Page Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
 // En tu archivo Home.jsx
 import React from 'react';
+import mermeluchosBG from '../assets/MermeluchosBG.webp';
+import verify from '../assets/Perrenque.png';
+
+/* import estrellas from '../assets/EstrellasBG.png'; */	
 
 const Home = () => {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold mb-6">Bienvenidos a Mermelucho</h1>
-      <p className="mb-4">Descubre nuestros productos artesanales...</p>
-      
-      {/* Más contenido aquí */}
+    <div className="p-6 mermeluchos-container">
+
+        <img 
+        src={mermeluchosBG} 
+        alt="Fondo decorativo Mermeluchos" 
+        className="w-full max-w-5xl mx-auto rounded-xl shadow-lg mermeluchos-img"
+      />
+
+        <h1 className="main-title">BIENVENIDOS A LA <span className="mermepagina">MERMEPÁGINA</span> WEB</h1> 
+        <div className="containerBorder">
+            <img src={verify} 
+            alt="Fondo decorativo Mermeluchos" 
+            className="w-full max-w-5xl mx-auto rounded-xl shadow-lg verificado-img" />
+        </div>
+
+        <h2 className="second-subtitle"><span className="phraseMark">"</span>Sabores que nacen de la tierra, endulzados con tradición.</h2>
     </div>
   );
 };
